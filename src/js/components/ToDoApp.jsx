6e782f243678dcaa917008805b7b69b35c2872a0
MFriasSaavedra <martin.frias.saavedra@gmail.com>
2025-07-31 15:48:04 +0000

    hover X-counter ok-style fix

--- src/js/components/ToDoApp.jsx
+++ src/js/components/ToDoApp.jsx
@@ -1,4 +1,4 @@
-import react, {useState} from 'react'
+import react, { useState } from 'react'
 function ToDoApp() {
     const [todos, setTodos] = useState([]);
     const [todo, setTodo] = useState('');
@@ -19,38 +19,45 @@ function ToDoApp() {
         setTodos(todos.filter((_, i) => i !== index));
     };
 
+    let counter;
+    if (todos.length === 1) {
+        counter = '1 tarea pendiente';
+    } else {
+        counter = `${todos.length} tareas pendientes`;
+    }
     return (
         // GENERAR COPN BOOTSTRAP!!!!
-        <div className= "to-do-container"> 
-            <h1 className="title">to do!</h1>
+        <div className="container">
+        <div className="to-do-container ">
+            <h1 className="title">To Do List</h1>
             <form onSubmit={handleSubmit}>
                 <input
                     type="text"
-                    className="input-task"
+                    className="form-control mb-3"
                     placeholder="¿Qué tareas necesitas hacer?"
                     value={todo}
                     onChange={handleChange}
                 />
             </form>
-           {/* // GENERAR COPN BOOTSTRAP!!!! */}
-            <ul className="task-list">
+
+            <ul className="task-list ">
                 {todos.length === 0 ? (
-                    <li className="no-task">Aún sin tareas, ¡Ingresa tus pendientes!.</li>
+                    <li className="no-task">Aún sin tareas, ¡Ingresa tus pendientes!.
+                    </li>
                 ) : (
                     todos.map((todo, index) => (
-                        <li key={index} className="task-item">
+                        <li key={index} className="task-item  d-flex justify-content-between align-items-center">
                             <span>{todo}</span>
-                            <button
-                                className="delete-button"
+                            <button type="button" className="btn  btn-sm delete-button"
                                 onClick={() => handleDelete(index)}
-                            >
-                                
-                           CLICK! {/* CLICK! poner icono! */}
-                            </button>
+                            >X</button>
+
                         </li>
                     ))
                 )}
             </ul>
+            <p className="todo-count"> {counter} </p>
+        </div>
         </div>
     );
 }
